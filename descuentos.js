cb4ccc2b89e8705e635fd00a5e2fbf147f202252
luid95 <luisdavidmorales95@gmail.com>
2021-10-20 08:13:40 -0500

    Ajuste html y js descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,22 +1,32 @@
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const porcentajeConDescuento  = 100 - descuento;
 
-const precioConDescuento = (precioOriginal * porcentajeConDescuento) / 100;
-
-console.log({
-    precioOriginal,
-    descuento,
-    porcentajeConDescuento,
-    precioConDescuento
-});
-
-//Descuento con funciones
-function precioConDescuento(precio, descuento){
-    const porcentajeConDescuento  = 100 - descuento;
-
-    const precioConDescuento = (precio * porcentajeConDescuento) / 100;
-
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
     return precioConDescuento;
-}
+  }
+  
+  function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
+  
+  
+  // console.log({
+  //   precioOriginal,
+  //   descuento,
+  //   porcentajePrecioConDescuento,
+  //   precioConDescuento,
+  // });
+  
